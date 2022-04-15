23023704b8d6a689ca676a16106ad76e1d9a1473
Dewa Putu Surya Adhigamika <suryaadhigamika@student.unud.ac.id>
2022-04-16 00:23:20 +0800

    midtest

--- resources/views/Templates/template.blade.php
+++ resources/views/Templates/template.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/layout.css">
 
     <title>@yield('title')</title>
   </head>
@@ -21,7 +22,7 @@
         <li class="nav-item mt-3">
             <form action="{{route('admin.logout')}}" method="post">
                 @csrf
-                <button type="submit" class="btn btn-dark" >Logout</button>
+                <button type="submit" class="btn btn-dark admin" >Logout</button>
             </form>
         </li>
       </ul>
